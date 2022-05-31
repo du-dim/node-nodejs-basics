d04a2b614ff9c123c92403b2baba414f6f13b5f4
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-05-31 15:18:22 +0300

    feat: solve copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve();
+const fsp = fs.promises;
+let linkFile1 = path.join(__dirname, 'files');
+let linkFile2 = path.join(__dirname, 'files_copy');
+
 export const copy = async () => {
-    // Write your code here 
-};
+    try {		
+		if (!fs.existsSync(linkFile1) || fs.existsSync(linkFile2)) throw new Error('FS operation failed');  
+        await copyDir(linkFile1, linkFile2)
+	} catch (err) {
+		console.log(err.message);
+	}
+}
+
+async function copyDir(a, b) {
+	const files = await fsp.readdir(a, { withFileTypes: true })
+	await fsp.mkdir(b, { recursive: true });
+	files.forEach(f => {
+		if (f.isFile()) {
+        let myReadStream = fs.createReadStream(path.join(a, f.name));
+		let myWriteStream = fs.createWriteStream(path.join(b, f.name));
+		myReadStream.pipe(myWriteStream);
+		myReadStream.on('error', (err) => console.log(err.message));
+		myWriteStream.on('error', (err) => console.log(err.message));
+    } else  {
+			copyDir(path.join(a, f.name), path.join(b, f.name)); 
+		}           
+  })
+}
+copy();
