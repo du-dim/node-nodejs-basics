9b69a781cd1c9b35a0b459f7c473b83c0e44b87a
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-06-01 00:00:10 +0300

    feat: solve args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,7 @@
+const args = process.argv.slice(2);
+
 export const parseArgs = () => {
-    // Write your code here 
+    const arr = args.join(' ').split('--').slice(1).map(arg => arg.trim().split(' ').join(' is ')); 
+    console.log(arr.join(', '));
 };
+parseArgs();
