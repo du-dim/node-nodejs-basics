8af533d405745d0c6e7b3c12b60b1db4efb6b597
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-05-31 17:06:53 +0300

    feat: solve delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve();
+const fsp = fs.promises;
+const linkFile = path.join(__dirname, 'files', 'fileToRemove.txt');
+
 export const remove = async () => {
-    // Write your code here 
+    try {		
+		if (!fs.existsSync(linkFile)) throw new Error('FS operation failed');
+        await fsp.rm(linkFile, { recursive: true });
+	} catch (err) {
+		console.log(err.message);
+	} 
 };
+remove();
