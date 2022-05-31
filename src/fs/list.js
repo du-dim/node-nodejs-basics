9a359fcd5df587762726e1b66fed32b65bec14eb
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-05-31 17:48:40 +0300

    feat: solve list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve();
+const fsp = fs.promises;
+const linkFile = path.join(__dirname, 'files');
+const arrFileName = [];
+
 export const list = async () => {
-    // Write your code here 
+    try {		
+		if (!fs.existsSync(linkFile)) throw new Error('FS operation failed');
+        const data = await fsp.opendir(linkFile);
+        for await (const file of data) {
+            arrFileName.push(file.name);
+        }
+        console.log(arrFileName);
+                
+	} catch (err) {
+		console.log(err.message);
+	}
 };
+list();
