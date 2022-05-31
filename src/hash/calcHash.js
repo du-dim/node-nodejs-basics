8580fb100f64e7aca8d54041ab8c03346bd924ad
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-06-01 02:18:38 +0300

    feat: solve calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+const __dirname = path.resolve();
+const fsp = fs.promises;
+const linkFile = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const data = await fsp.readFile(linkFile, 'utf8');
+    return crypto.createHash('sha256').update(data).digest('hex');    
 };
+
+console.log(await calculateHash());
