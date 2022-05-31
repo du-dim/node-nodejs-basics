9fe939a71d72e372be00fd4f30b5102fde9e68f0
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-05-31 18:17:51 +0300

    feat: solve read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve();
+const fsp = fs.promises;
+const linkFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
 export const read = async () => {
-    // Write your code here 
-};
+    try {		
+		if (!fs.existsSync(linkFile)) throw new Error('FS operation failed');
+        const data = await fsp.readFile(linkFile, 'utf8');  
+        console.log(data);
+	} catch (err) {
+		console.log(err.message);
+	}
+}
+read();
