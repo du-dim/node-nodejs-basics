75423dbcd86d0b907b33a79189bd59f3e820e5fe
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-05-31 18:29:19 +0300

    feat: solve rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve();
+const fsp = fs.promises;
+const linkFile1 = path.join(__dirname, 'files', 'wrongFilename.txt');
+const linkFile2 = path.join(__dirname, 'files', 'properFilename.md');
+
 export const rename = async () => {
-    // Write your code here 
+    try {		
+		if (!fs.existsSync(linkFile1) || fs.existsSync(linkFile2)) throw new Error('FS operation failed');  
+        await fsp.rename(linkFile1, linkFile2);
+	} catch (err) {
+		console.log(err.message);
+	}
 };
+rename();
