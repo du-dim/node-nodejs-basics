7396cd4b8dccc8c5c3f861affe6a7818e175706b
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-05-31 20:39:18 +0300

    feat: solve env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,9 @@
-export const parseEnv = () => {
-    // Write your code here 
+const data = process.env;
+const list = [];
+export const parseEnv = () => {     
+    for (const key in data) {
+        if (key.includes('RSS_')) list.push(`${key} = ${data[key]}`);
+    }    
+    list.length ? console.log(list.join('; ')) : false;
 };
+parseEnv();
