4b3ebc57a3f4f8d2727edefdc91198227c92c39e
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-06-01 20:11:24 +0300

    feat: write streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+const __dirname = path.resolve();
+const linkFile = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 export const write = async () => {
-    // Write your code here 
+    const myWriteStream = fs.createWriteStream(linkFile, 'utf8');
+    process.stdin.on('data', data => {        
+        myWriteStream.write(data.toString());
+        process.exit();
+    });
 };
+write();
