9a8afe15e7e0809a439933ecc57777f0a87a8493
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-06-01 21:11:31 +0300

    feat: transform streams

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,19 @@
+import { Transform, pipeline } from 'stream';
+
+const readable = process.stdin;
+const writable = process.stdout;
+
+const myTransform = new Transform({
+    transform(chunk, encoding, callback) {
+        const chunkData = chunk.toString();
+        const chunkTransform = chunkData; // doesn't transform
+        callback(null, chunkTransform);
+        process.exit();
+    }
+});
+
 export const transform = async () => {
-    // Write your code here 
+    pipeline(readable, myTransform, writable, err => console.error(`${err}`));
+    
 };
+transform();
