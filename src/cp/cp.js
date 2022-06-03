b6ca663ae50c1665e490462e1ea6537235236b66
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-06-03 23:38:35 +0300

    feat: solve cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,18 @@
+import { spawn } from 'child_process';
+import path from 'path';
+const linkFile = path.join('files', 'script.js');
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node ' + linkFile, args, {shell: true});
+    child.stdout.on('data', data => {        
+        console.log(data.toString());            
+    }); 
+    process.stdin.on('data', data => {                     
+        child.stdin.write(data);        
+    });
+    child.on('close', () => {
+        process.exit();
+    });                  
 };
+
+spawnChildProcess(['Test1', 'Test2', 'Test3']);
