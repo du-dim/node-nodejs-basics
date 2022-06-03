454231fc78ab66415bb8635025b0aee0c5c80f20
Дмитрий Дубовик <dim-dubovik@yandex.ru>
2022-06-03 03:27:24 +0300

    feat: solve wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,29 @@
+import os from 'os';
+import { Worker } from 'worker_threads';
+import path from 'path';
+const __dirname = path.resolve();
+const linkFile = path.join(__dirname, 'worker.js');
+
 export const performCalculations = async () => {
-    // Write your code here
+    const cpus = os.cpus();
+    const arrPromise = cpus.map((_,i) => promise(i));
+    const arrResult = [];
+    await Promise.all(arrPromise).then(results => results.forEach(res => arrResult.push(res)));
+    console.log(arrResult);
 };
+
+const promise = (index) => new Promise((resolve, reject) => {
+    const worker = new Worker(linkFile, { workerData: 10 + index});             
+    worker.on('message', msg => { 
+        const obj = {status: 'resolve', data: msg};  
+        resolve(obj);    
+    }); 
+    worker.on('error', err => { 
+        const obj = {status: 'error', data: null}; 
+        resolve(obj);                                                 
+    });  
+})  
+
+performCalculations();
+
+            
